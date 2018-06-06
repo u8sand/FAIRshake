b19c2cc433b9afe744d234a3155786d71fec10cb
Daniel J. B. Clarke <u8sand@gmail.com>
2018-06-06 13:16:23 -0400

    Added implements decorator for asserting proper interface implementation

--- app/util/decorators.py
+++ app/util/decorators.py
@@ -14,3 +14,22 @@ def interface(iface):
     if callable(attr):
       assert attr.__doc__, '%s does not have a docstring' % (iface)
   return iface
+
+def implements(iface):
+  ''' A decorator for asserting that an implementation fully implements its interface/model.
+  '''
+  def implements_decorator(impl):
+    try:
+      setattr(impl, '__doc__', iface.__doc__)
+    except:
+      pass
+    for attr in dir(iface):
+      if attr.startswith('_'):
+        continue
+      assert getattr(impl, attr, None) is not None, '%s does not implement %s.%s' % (impl.__name__, iface.__name__, attr)
+      try:
+        setattr(getattr(impl, attr), '__doc__', getattr(iface, attr).__doc__)
+      except:
+        pass
+    return impl
+  return implements_decorator
