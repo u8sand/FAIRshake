1aa860d6295c7a53dd54e15cf5c677fc4e313aa5
Daniel J. B. Clarke <u8sand@gmail.com>
2018-06-12 18:26:17 -0400

    Added implements to mocks

--- app/entities/Assessment/mock.py
+++ app/entities/Assessment/mock.py
@@ -1,6 +1,8 @@
-from interfaces.Assessment import AssessmentModel
+from interfaces.Assessment import AssessmentAPI, AssessmentModel
 from util.types import HTTPResponse, UUID, Timestamp, Optional, List
+from util.decorators import implements
 
+@implements(AssessmentAPI)
 class MockAssessmentAPI:
   def get(
       id: Optional[UUID] = None,
--- app/entities/Score/mock.py
+++ app/entities/Score/mock.py
@@ -1,6 +1,8 @@
-from interfaces.Score import ScoreModel
+from interfaces.Score import ScoreAPI, ScoreModel
 from util.types import Any, UUID, Timestamp, ContentType, HTTPResponse
+from util.decorators import implements
 
+@implements(ScoreAPI)
 class MockScoreAPI:
   def get(id: UUID, kind: ContentType) -> HTTPResponse[Any]:
     return '<svg viewBox="0 0 1 1" />' if kind == 'text/html' else ScoreModel(
