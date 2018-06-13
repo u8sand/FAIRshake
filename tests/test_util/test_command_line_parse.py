e83fb52a31cf04323f9ece3df668563559a79336
Daniel J. B. Clarke <u8sand@gmail.com>
2018-06-13 13:51:30 -0400

    Added command-line parser to utils

---
+++ tests/test_util/test_command_line_parse.py
@@ -0,0 +1,8 @@
+from app.util.command_line_parse import parse
+
+def test_parse():
+  assert parse(
+    ['a', '10', '--c=d', 'true', '--g=h i', '--j=false']
+  ) == (
+    ['a', 10, True,], {'c': 'd', 'g': 'h i', 'j': False,}
+  )
