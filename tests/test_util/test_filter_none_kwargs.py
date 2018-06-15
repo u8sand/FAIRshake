b7f8f17fd5b62568dc7986d207b07385add0acda
Daniel J. B. Clarke <u8sand@gmail.com>
2018-06-15 11:42:53 -0400

    Minor util improvements

--- tests/test_util/test_filter_none_kwargs.py
+++ tests/test_util/test_filter_none_kwargs.py
@@ -2,3 +2,4 @@ from app.util.filter_none_kwargs import filter_none_kwargs
 
 def test_filter_none_kwargs():
   assert filter_none_kwargs(a=None, b='c') == {'b': 'c'}
+  assert filter_none_kwargs({'a': None, 'b': 'c'}, d=None, e='c') == {'b': 'c', 'e': 'c'}
